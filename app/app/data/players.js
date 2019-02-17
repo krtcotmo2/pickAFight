da94b1aa70e88fbc9d59dc8d57fd7f116d5c56fe
Kurt Cooney <krtcotmo2@gmail.com>
2019-02-17 18:46:25 -0500

    Added first structure for data

---
+++ app/app/data/players.js
@@ -0,0 +1,19 @@
+let players = [
+     {
+          name : "Steve Rogers",
+          alias : "Captain Aamerica",
+          skill:[{
+               durability: 3,
+               energy: 1,
+               fighting:6,
+               intel:3,
+               speed:2,
+               strength:3,
+               gizmos:2
+          }],
+          side:1,
+          profImg:"url to image",
+          profDir:1,
+          img:"url to image"
+     }
+]
